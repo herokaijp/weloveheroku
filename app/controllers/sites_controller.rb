42be9336b113e3e67d3d1f323383ca807a91d8be
Takashi Hayasaka <iwhurtafly@gmail.com>
2013-06-08 20:01:32 +0900

    Adding create page

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,4 +2,19 @@ class SitesController < ApplicationController
   def new
     @site = Site.new
   end
+  
+  def create
+    @site = Site.new(site_params)
+    @site.save
+  end
+  
+  private
+  def site_params
+    params.require(:site).permit(
+      :name, :url, :description,
+      :creator, :hash_tag, :repository_url,
+      :please_design
+    )
+  end
+  
 end
--- spec/features/sites_spec.rb
+++ spec/features/sites_spec.rb
@@ -8,5 +8,22 @@ describe "Sites" do
     it '画面が表示されること' do
       page.should have_content('New site')
     end
+
+    describe '登録する' do
+      let(:new_site) { FactoryGirl.build(:site) }
+      before do
+        find('#site_name').set(new_site.name)
+        find('form#new_site input[type=submit]').click()
+      end
+      
+      it '登録完了画面へ遷移すること' do
+        current_path.should eq sites_path
+      end
+
+      it '登録完了メッセージに登録したサイトの名前が含まれること' do
+        page.should have_content("#{new_site.name}")
+      end
+
+    end
   end
 end
