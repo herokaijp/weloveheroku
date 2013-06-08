c83642c202dda7e0d74580f1e0fa4711116773e4
koshikawa <koshikawa@ppworks.jp>
2013-06-08 14:02:02 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Genuineblue::Application.config.secret_key_base = 'daca2729939898d8ab6f547790e1b7d0a800fae4c04bbd592c222654283b10f0b86c2c466751c9badd51002ce28a8d610de41401d6ae0a51a8b84cdca8c884b3'
